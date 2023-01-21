65c6ba0ba29d3f2c707bbc42fa6a5b2b0e72e9c5
Максим <gmax7911@gmail.com>
2023-01-21 12:50:47 +0300

    Hi

---
+++ src/Transport/Car.java
@@ -0,0 +1,48 @@
+package Transport;
+
+import Drivers.DriverB;
+
+public class Car extends Transport<DriverB> implements Racing {
+
+    public enum BodyType {
+        SEDAN("Седан"),
+        HATCHBACK("Хетчбэк"),
+        COUPE("Купе"),
+        WAGON("Универсал"),
+        OFFROAD("Внедорожник"),
+        CROSSOVER("Кроссовер"),
+        PICKUP("Пикап"),
+        VAN("Фургон"),
+        MINIVAN("Минивэн");
+
+        private final String bodyType;
+
+        BodyType(String bodyType) {
+            this.bodyType = bodyType;
+        }
+
+        public String getBodyType() {
+            return bodyType;
+        }
+
+        @Override
+        public String toString() {
+            return "Тип кузова - " + bodyType;
+        }
+    }
+    public Car(String brand, String model, double engineVolume, DriverB driver) {
+        super(brand, model, engineVolume, driver);
+    }
+    public void getPitStop(Car car) {
+        System.out.println(car.getBrand() + " " + car.getModel() + " - съехал с дистанции на питстоп!");
+    }
+    public void getBestTimeLap(Car car) {
+        System.out.println(car.getBrand() + " " + car.getModel() + " - Лучшее время круга!");
+    }
+    public void getMaxSpeed(Car car) {
+        System.out.println(car.getBrand() + " " + car.getModel() + " - Набрал максимальную скорость!");
+
+    }
+
+}
+
